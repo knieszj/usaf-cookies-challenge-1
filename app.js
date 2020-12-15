5f5b98900295113dcf40ce047e506c6d3b839a1e
Josef Kniesz <knieszj@jdi.socom.mil>
2020-12-15 08:42:50 -0500

    got cookies to work in a basic way.

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser())
 
 
+app.get('/login', (req, res) => {
+    if (req.query.username !== undefined){
+        let cookieUserNameArray = Object.values(req.query)
+        res.cookie('name', `${cookieUserNameArray[0]}`)
+        res.status(200).json({message: `Logged in as: ${cookieUserNameArray[0]}`})
+    }   else {
+        res.status(404).json({message: `Something went wrong.  Please query with /?username=name`})
+    }
+})
 
 
 const port = 3000
